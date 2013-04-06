8232add3a0659b3afe57a7211558546f20a4e973
alevellop <alevellop@gmail.com>
2013-04-06 22:25:56 +0200

    creando dependencias en el blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
 
+  belongs_to :user
+
   def long_title
 
   	"#{title} - #{published_at}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password
 
   has_one :profile
+
+  has_many :articles, order: "published_At DESC, title ASC", 
+  										depedent: :destroy
+
+  #TODO: continuar por la pagina 113/403 del pdf: antes de rails g migration create_articles_categories
 end
